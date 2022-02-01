43d421f49265e7930a69788bc7e2d274cadd0cf5
Martenstenden <marten.de.jong@student.nhlstenden.com>
2022-02-01 14:50:52 +0100

    iets

--- Exercises/Pages/Lesson1/Assignment1.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment1.cshtml.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment1 : PageModel
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+
+        public int scoreHome { get; set; }
+        public int scoreAway { get; set; }
+        public void OnGet([FromRoute] string action)
         {
+
+            if (action == "incrementHome") { scoreHome++; }
+            else if (action == "decrementHome") { scoreHome--; }
+            else if (action == "incrementAway") { scoreAway++; }
+            else if (action == "decrementAway") { scoreAway--; }
+            else if (action == "reset") { scoreAway = 0; scoreAway = 0; }
         }
     }
 }
