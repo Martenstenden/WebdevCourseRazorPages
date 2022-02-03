e8d4c6eb9e8de7c834f9e654ad13e0bbd6ad6c7d
Marten de Jong <91325504+Martenstenden@users.noreply.github.com>
2022-02-03 12:01:03 +0100

    Opdracht 2

--- Exercises/Pages/Lesson1/Assignment1.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment1.cshtml.cs
@@ -1,22 +1,48 @@
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment1 : PageModel
     {
-        [BindProperty(SupportsGet = true)]
-
         public int scoreHome { get; set; }
         public int scoreAway { get; set; }
-        public void OnGet([FromRoute] string action)
+        public void OnGet()
         {
+            if (Request.Query.ContainsKey("incrementHome"))
+            {
+                scoreHome = Convert.ToInt32(Request.Query["incrementHome"].First());
 
-            if (action == "incrementHome") { scoreHome++; }
-            else if (action == "decrementHome") { scoreHome--; }
-            else if (action == "incrementAway") { scoreAway++; }
-            else if (action == "decrementAway") { scoreAway--; }
-            else if (action == "reset") { scoreAway = 0; scoreAway = 0; }
+                scoreHome++;
+            }
+
+            else if (Request.Query.ContainsKey("decrementHome"))
+            {
+                scoreHome = Convert.ToInt32(Request.Query["decrementHome"].First());
+
+                scoreHome--;
+            }
+
+            else if (Request.Query.ContainsKey("incrementAway"))
+            {
+                scoreAway = Convert.ToInt32(Request.Query["incrementAway"].First());
+
+                scoreAway++;
+            }
+
+            else if (Request.Query.ContainsKey("decrementAway"))
+            {
+                scoreAway = Convert.ToInt32(Request.Query["decrementAway"].First());
+
+                scoreAway--;
+            }
+
+            else if (Request.Query.ContainsKey("reset"))
+            {
+                scoreHome = 0;
+                scoreAway = 0;
+            }
         }
     }
 }
