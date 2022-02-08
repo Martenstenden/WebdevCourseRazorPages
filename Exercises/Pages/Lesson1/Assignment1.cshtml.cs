a96fc16e4bb4d9ae4258132b7b2ecd97abdbdad6
Marten de Jong <91325504+Martenstenden@users.noreply.github.com>
2022-02-08 12:21:52 +0100

    iets

--- Exercises/Pages/Lesson1/Assignment1.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment1.cshtml.cs
@@ -1,48 +1,19 @@
-using System;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment1 : PageModel
     {
-        public int scoreHome { get; set; }
-        public int scoreAway { get; set; }
+        public int homeScore { get; set; }
+        public int awayScore { get; set; }
+        public string action { get; set; }
+
         public void OnGet()
         {
-            if (Request.Query.ContainsKey("incrementHome"))
-            {
-                scoreHome = Convert.ToInt32(Request.Query["incrementHome"].First());
-
-                scoreHome++;
-            }
-
-            else if (Request.Query.ContainsKey("decrementHome"))
-            {
-                scoreHome = Convert.ToInt32(Request.Query["decrementHome"].First());
-
-                scoreHome--;
-            }
-
-            else if (Request.Query.ContainsKey("incrementAway"))
-            {
-                scoreAway = Convert.ToInt32(Request.Query["incrementAway"].First());
-
-                scoreAway++;
-            }
-
-            else if (Request.Query.ContainsKey("decrementAway"))
-            {
-                scoreAway = Convert.ToInt32(Request.Query["decrementAway"].First());
-
-                scoreAway--;
-            }
-
-            else if (Request.Query.ContainsKey("reset"))
-            {
-                scoreHome = 0;
-                scoreAway = 0;
-            }
+            homeScore = Request.Query["ScoreHome"];
+            awayScore = Request.Query["ScoreAway"];
+            action = Request.Query["action"];
         }
     }
 }
