57d489d4576983c343f91067cce736a927cb15c9
Marten de Jong <91325504+Martenstenden@users.noreply.github.com>
2022-02-08 10:11:58 +0100

    update opdracht3

--- Exercises/Pages/Lesson1/Assignment3.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment3.cshtml.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
 
 namespace Exercises.Pages.Lesson1
 {
@@ -8,22 +6,7 @@ namespace Exercises.Pages.Lesson1
     {
         public void OnGet()
         {
-         
-        }
 
-        [BindProperty]
-        public string Result { get; set; }
-
-        public int intermediateresult = 0;
-
-        public void OnPost([FromForm] string input)
-        {
-            intermediateresult = Convert.ToInt32(input);
-            
-            if (intermediateresult != 0)
-            {
-                Result = intermediateresult + input;
-            }
         }
     }
 }
