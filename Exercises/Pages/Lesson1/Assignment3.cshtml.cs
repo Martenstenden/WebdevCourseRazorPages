f65e5bac1774d9e88392de6d926b6a367c9a6e1d
Martenstenden <marten.de.jong@student.nhlstenden.com>
2022-02-08 15:50:51 +0100

    opdracht 3

--- Exercises/Pages/Lesson1/Assignment3.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment3.cshtml.cs
@@ -1,12 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment3 : PageModel
     {
+        public int Resultaat { get; set; }
+
+        [BindProperty(Name = "input")]
+        public int InputBindProperty { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
         }
     }
 }
