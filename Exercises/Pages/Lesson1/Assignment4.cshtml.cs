d729dfadfc32ce2bdf8c08218359066eb054f876
Martenstenden <marten.de.jong@student.nhlstenden.com>
2022-02-15 18:10:10 +0100

    assignment 4 en 5

--- Exercises/Pages/Lesson1/Assignment4.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment4.cshtml.cs
@@ -1,11 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment4 : PageModel
     {
-        public void OnGet()
+        public string h1title { get; set; }
+        public string h2title { get; set; }
+        public string h3title { get; set; }
+        public void OnGet([FromRoute] string category, [FromRoute] string subcategory, [FromRoute] string productid)
         {
+            if (subcategory == "subcategory")
+            {
+                h1title = category;
+                h2title = "geen subcategory";
+                if (productid == "0")
+                {
+                    h3title = "geen productid";
+                }
+                else
+                {
+                    h3title = productid;
+                }
+            }
+            else
+            {
+                h1title = category;
+                h2title = subcategory;
+                if (productid == "0")
+                {
+                    h3title = "geen productid";
+                }
+                else
+                {
+                    h3title = productid;
+                }
+            }
 
         }
     }
--- Exercises/Pages/Lesson1/Assignment5.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment5.cshtml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics.Metrics;
 using Exercises.Pages.Lesson1.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
@@ -11,8 +14,48 @@ namespace Exercises.Pages.Lesson1
             public int Angry { get; set; }
         }
 
-        public void OnGet()
+        public int Happy { get; set; } = 0;
+        public int Disappointed { get; set; } = 0;
+        public int Angry { get; set; } = 0;
+
+        public void OnGet(string action = "")
         {
+            action = action.ToLower();
+
+            string counterStr = Request.Cookies["happy"];
+            if (counterStr == null)
+            {
+                Response.Cookies.Append("happy", Happy.ToString(), new CookieOptions()
+                {
+                    Expires = DateTimeOffset.Now.AddDays(30)
+                });
+            }
+            else
+            {
+                Happy = int.Parse(counterStr);
+                Disappointed = int.Parse(counterStr);
+                Angry = int.Parse(counterStr);
+                
+                if (action == "happy")
+                {
+                    Happy++;
+                    Response.Cookies.Append("happy", Happy.ToString());
+                }
+                else if (action == "disappointed")
+                {
+                    Disappointed++;
+                    Response.Cookies.Append("counter", Disappointed.ToString());
+                }
+                else if (action == "angry")
+                {
+                    Angry++;
+                    Response.Cookies.Append("counter", Angry.ToString());
+                }
+                else if (action == "reset")
+                {
+                    Response.Cookies.Delete("counter");
+                }
+            }
         }
     }
 }
